46a962b6c2ceb16706f742af7a57b96066fc07c1
StefanPDavis <snoodle22@hotmail.com>
2024-01-17 15:18:30 -0800

    added question for title

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,14 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        prompt: "What is the title of your new project?"
+
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
