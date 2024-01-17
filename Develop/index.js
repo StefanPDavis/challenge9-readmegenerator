106c3631c838f77a8a718051811d7edbbc6eb71e
StefanPDavis <snoodle22@hotmail.com>
2024-01-17 15:33:50 -0800

    added question and list for licenses

--- Develop/index.js
+++ Develop/index.js
@@ -6,37 +6,37 @@ const questions = [
         type: "input",
         name: "title",
         prompt: "What is the title of your new project?"
-
     },
     {
         type: "input",
         name: "title",
         prompt: "What is the title of your new project?"
-
     },
     {
         type: "input",
         name: "description",
         prompt: "Type a short description about your new project."
-
     },
     {
         type: "input",
         name: "Usage",
         prompt: "What do users need to know about your new project's repository?"
-
     },
     {
         type: "input",
         name: "contribution",
         prompt: "What do users need to know about contributing to your new project's repository?"
-
     },
     {
         type: "input",
         name: "test",
         prompt: "What command should be used to run a test?"
-
+    },
+    {
+        type: "list",
+        name: "license",
+        prompt: "Which license would you like for your new project?",
+        choices: ["Apache", "MIT", "Boost Software", "Eclipse Public", "Mozilla Public", "The Unlicense", "none"]
     }
 ];
 
